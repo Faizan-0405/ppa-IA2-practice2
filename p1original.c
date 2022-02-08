4fdeddfc51f892e65b568473fec12db1c67431d4
Faizan-0405 <94587991+Faizan-0405@users.noreply.github.com>
2022-02-08 07:11:55 +0000

    nill

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(float *base ,float *height)
+{
+  printf("enter the height and base\n");
+  scanf("%f%f",base,height);
+}
+void find_area(float base ,float height,float *area)
+{
+  *area=(base*height)/2;
+}
+void output(float base,float height,float area)
+{
+  printf("the area of trinagle with %f and height %f is %f",base,height,area);
+}
+int main()
+{
+  float a,b,c;
+  input(&a,&b);
+  find_area(a,b,&c);
+  output(a,b,c);
+  //return 0;
 }
